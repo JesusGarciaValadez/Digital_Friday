0ad3db8cb492317ddaffff65be05d33ff6e50e0d
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2014-03-04 11:58:11 -0600

    Verification of connection on production server
    Signed-off-by: Jesús Antonio García Valadez <zerovamp@hotmail.com>

--- Code/snippets/db/connection.php
+++ Code/snippets/db/connection.php
@@ -2,8 +2,7 @@
 try {
     $dbh = new PDO('mysql:host=localhost;dbname=cmv', 'CMV4sf4l70', '{AmKZQvX3dzbcAVpc)qe8Kei');
     $dbh->exec("SET CHARACTER SET utf8");
-}
-catch ( PDOException $e ){
+} catch ( PDOException $e ) {
     print "Error!: " . $e->getMessage() . "<br/>";
     die();
 }
